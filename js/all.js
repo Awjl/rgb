832ffab98e2712f66236930e4c6b626ae054e079
佳乐 <le179516844@163.com>
2018-06-16 16:53:41 +0800

    项目完成

--- js/all.js
+++ js/all.js
@@ -1,10 +1,26 @@
 $(window).scroll(function () {
-  console.log("aaa")
-  if ($(".ourslist").offset().top - $("body").scrollTop() <= 0) {
-    $(".ourslist ul").addClass(".animated");
-    console.log("bbb")
-  } else {
-    $(".ourslist ul").addClass(".animated");
-    console.log("ccc")
+  if($(window).scrollTop()+$(window).height()>$(".title-one").offset().top) {
+    $(".title-one").css({opacity:"1"});
   }
+  if($(window).scrollTop()+$(window).height()>$(".title-two").offset().top ) {
+    $(".title-two ").css({opacity:"1"});
+  }
+  if($(window).scrollTop()+$(window).height()>$(".title-three").offset().top) {
+    $(".title-three").css({opacity:"1"});
+  }
+  if ($(window).scrollTop()+$(window).height()>$(".ourslist").offset().top) {
+    $(".ourslist ul").addClass("animated");
+  } 
+  if ($(window).scrollTop()+$(window).height()>$(".welfare").offset().top) {
+    $(".welfare ul").addClass("animated");
+  } 
+  if ($(window).scrollTop()+$(window).height()>$(".recruit-list").offset().top) {
+    $(".recruit-list").addClass("animatedDiv");
+  } 
+  $(".recruit-list>div").click(function() {
+    $(this).addClass("all-recruit ")
+  })
+  $("#done").click(function() {
+    $("div").removeClass('all-recruit')
+  })
 })
